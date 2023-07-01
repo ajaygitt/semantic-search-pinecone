eab390e9f161997d7c9b6a15a32e8470e6be2a8c
ajaypradeep <ajaypradeep1712@gmail.com>
2023-07-01 17:27:26 +0530

    semantic search

---
+++ test.js
@@ -0,0 +1,36 @@
+import { Configuration, OpenAIApi } from 'openai'
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log(process.env.OPENAI_API_KEY)
+const config = new Configuration({
+	apiKey: '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'
+});
+
+const openai = new OpenAIApi(config);
+
+const runPrompt = async () => {
+	const prompt = `
+        write me a joke about a cat and a bowl of pasta. Return response in the following parsable JSON format:
+        {
+            "Q": "question",
+            "A": "answer"
+        }
+    `;
+
+	const response = await openai.createCompletion({
+		model: "text-davinci-003",
+		prompt: prompt,
+		max_tokens: 2048,
+		temperature: 1,
+	});
+
+	const parsableJSONresponse = response.data.choices[0].text;
+	const parsedResponse = JSON.parse(parsableJSONresponse);
+
+	console.log("Question: ", parsedResponse.Q);
+	console.log("Answer: ", parsedResponse.A);
+};
+
+runPrompt();
